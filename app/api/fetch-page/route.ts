c5a748a0faf70d733dda5ef111e3a1a880b71caf
s4mzai <hi.samar.7x@gmail.com>
2025-12-30 19:50:10 +0530

    Add fetch-page api to fetch html of a webPage

---
+++ app/api/fetch-page/route.ts
@@ -0,0 +1,39 @@
+import { isValidUrl } from "@/lib/isValidUrl"
+import axios from "axios"
+import { NextResponse } from "next/server"
+
+export async function POST(req:Request){
+    try{
+        const body = await req.json()
+        const { url } = body
+
+        if(!url || !isValidUrl(url)){
+            return NextResponse.json(
+                { error: "Invalid Url" },
+                { status: 400 }
+            )
+        }
+        
+        const response = await axios.get(url, {
+            headers: {
+              "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+              "Accept": "text/html",
+            },
+            timeout: 10000,
+            maxRedirects: 5,
+        })
+
+        const html = response.data
+
+        return NextResponse.json({
+            success:true,
+            html,
+        })
+    }catch (error){
+        return NextResponse.json(
+            { error: "Failed to fetch page" },
+            { status: 500 } 
+        )
+    }
+}
